68cb4e55867d616ff52b7bbb6e9c372ad83c93b2
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-02-13 17:35:08 -0500

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
 <Iframe url="https://the-house-contabilidad.vercel.app"
-        width="100%"
-        height="740px"
-        className={styles.iframe}
-        allowFullScreen 
-        styles={{border: "0px"}}
-        display="block"
-        position="relative"/>
+        className={styles.container}/>
         </div>
   )
 }
